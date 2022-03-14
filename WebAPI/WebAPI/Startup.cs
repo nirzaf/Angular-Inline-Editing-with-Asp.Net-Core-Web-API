af73e0dda36a3009b6a98bfcd0ea33b436af1138
M F M Fazrin <mfmfazrin1986@gmail.com>
2022-03-14 10:02:12 +0530

    sdsdsds

--- WebAPI/WebAPI/Startup.cs
+++ WebAPI/WebAPI/Startup.cs
@@ -20,7 +20,8 @@ namespace WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+           // services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
 
             services.AddDbContext<APIDBContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
         }
@@ -46,7 +47,8 @@ namespace WebAPI
             options.WithOrigins("http://localhost:4200")
             .AllowAnyMethod()
             .AllowAnyHeader());
-            app.UseMvc();
+
+            //app.UseMvc();
         }
     }
 }
